f9f094f56b40fbe23e65aa37438f06d7c34e2139
Aniket Yadav <iloveme6306@gmail.com>
2026-06-22 23:56:25 +0530

    Time: 1 ms (24.33%), Space: 9 MB (64.87%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(std::string text) {
+        
+        std::unordered_map<char, int> textMap;
+        for (char c : text) {
+            textMap[c]++;
+        }
+
+        if (textMap.count('b') == 0 || 
+            textMap.count('a') == 0 || 
+            textMap.count('l') == 0 || 
+            textMap.count('o') == 0 || 
+            textMap.count('n') == 0) {
+            return 0; 
+        }
+
+        int bCount = textMap['b'] / 1; 
+        int aCount = textMap['a'] / 1; 
+        int lCount = textMap['l'] / 2; 
+        int oCount = textMap['o'] / 2; 
+        int nCount = textMap['n'] / 1;
+
+        return std::min({bCount, aCount, lCount, oCount, nCount});
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
