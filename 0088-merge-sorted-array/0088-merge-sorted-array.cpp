ab3cea472329bca6a60d57219dae80b24ba27128
Aniket Yadav <iloveme6306@gmail.com>
2026-06-24 23:17:07 +0530

    Time: 0 ms (100%), Space: 12.3 MB (72.26%) - LeetHub - Time: O(m + n), Space: O(m + n)

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> temp;
+        int i = 0;
+        int j = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                temp.push_back(nums1[i]);
+                i++;
+            } else {
+                temp.push_back(nums2[j]);
+                j++;
+            }
+        }
+
+        while (i < m) {
+            temp.push_back(nums1[i]);
+            i++;
+        }
+
+        while (j < n) {
+            temp.push_back(nums2[j]);
+            j++;
+        }
+
+        nums1 = temp;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
